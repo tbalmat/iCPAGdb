b8a282c3a8ab9107e5a07fc8ae334c994178729b
Tom Balmat <38592571+tbalmat@users.noreply.github.com>
2021-03-25 13:34:24 -0400

    Create iCPAGdb.r

---
+++ v1.0/iCPAGdb.r
@@ -0,0 +1,19 @@
+# Duke University Cross Phenotype Analysis of GWAS
+# Explore page, Novenber 2020
+
+# Launch app ui.r and server.r from specified appDir
+# Note the specification of a tcp port that the process will listen on for http requests
+
+library("shiny")
+
+# Specify directory containing ui.r and server.r
+
+# Local dir
+ad <- "C:/Projects/Duke/H2P2GenomeWideAssociationStudy/CPAG/iCPAGdb/App-Devel/App"
+
+# Execute 
+runApp(appDir=ad,
+       launch.browser=T,
+       host = getOption("shiny.host", "127.0.0.1"),
+       port=4295,
+       display.mode = c("auto", "normal", "showcase"))
